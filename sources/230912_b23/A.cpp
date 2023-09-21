d5234c5a254f4f4a67f4d81e9b54c6b1cfe7a5c7
Максимилиан <max4tim@bk.ru>
2023-09-21 14:38:58 +0300

    изучаем язык A

--- sources/230912_b23/A.cpp
+++ sources/230912_b23/A.cpp
@@ -5,8 +5,28 @@
  * @date 2023-09-21
  */
 
-#include "iostream"
+#include <cstdio>
+
+int abs(int a) {
+    return a < 0 ? -a : a;
+}
+
+int mulsgn(int n, int sgn) {
+    return sgn == 0 ? 0 : sgn < 0 ? -n : n;
+}
 
 int main() {
-    std::cout << "Maximilian" << std::endl;
+    int a = 0;
+    int b = 0;
+    int d = 0;
+
+    scanf("%d %d %d", &a, &b, &d);
+
+    int target = b - a;
+
+    if (abs(target) <= d)
+        printf("%d", b);
+    else
+        printf("%d", a + mulsgn(d, target));
+
 }
