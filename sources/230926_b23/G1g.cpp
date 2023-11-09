96821a02c4ebea66f4124aacd4e48c87b45ec707
Максимилиан <max4tim@bk.ru>
2023-11-09 14:31:48 +0300

    числа Гранди 1g черновик

--- sources/230926_b23/G1g.cpp
+++ sources/230926_b23/G1g.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <map>
 #include <set>
+#include <algorithm>
 
 using namespace std;
 
@@ -19,114 +20,36 @@ int mex(const set<int>& nums) {
     return -1;
 }
 
-class Fence {
-public:
+int countFenceGrand(short n) {
+    static int grandNums[300] = {};
+    static bool isGrandNumsCalced[300] = { 0};
 
-    Fence(const set<short>& seq) : stickSeq(seq) {};
+    if (isGrandNumsCalced[n]) return grandNums[n];
 
-    // all counting starts with 0
-    // all sizes starts with 1
+    int grandRes = 0;
 
-    size_t seqCount() const {
-        return stickSeq.size();
-    }
-
-    Fence push(int n, int nn, int downCount) const {
-        set<short> newSeq(stickSeq);
-
-        if (nn == 0 || nn == n - downCount) {
-            newSeq.erase(n);
-
-            if (n != downCount)
-                addSeq(newSeq, n - downCount);
-        } else {
-            newSeq.erase(n);
-
-            addSeq(newSeq, nn);
-            addSeq(newSeq, n - nn - downCount);
+    for (short downC = 1; downC <= 2 && downC <= n; ++downC) {
+        for (short nn = 0; nn <= n - downC; ++nn) {
+            grandRes ^= countFenceGrand(nn);
+            grandRes ^= countFenceGrand(n - nn - downC);
         }
-
-        return Fence(newSeq);
     }
 
-    bool operator<(const Fence& right) const {
-        return this->stickSeq < right.stickSeq;
+    grandRes = grandRes == 0 ? 1 : 0;
 
-        // if (this->stickSeq.size() < right.stickSeq.size()) return true;
-        // if (this->stickSeq.size() > right.stickSeq.size()) return false;
+    grandNums[n] = grandRes;
+    isGrandNumsCalced[n] = true;
 
-        // for (int i = 0; i < this->stickSeq.size(); ++i) {
-            // if (this->stickSeq[i] < right.stickSeq[i]) return true;
-            // if (this->stickSeq[i] > right.stickSeq[i]) return false;
-        // }
-
-        // return false;
-    }
-
-    const set<short>& getSeq() const {
-        return stickSeq;
-    }
-
-private:
-    const set<short> stickSeq;
-
-    static void addSeq(set<short>& seq, short n) {
-        if (seq.count(n))
-            seq.erase(n);
-        else
-            seq.emplace(n);
-    }
-};
-
-int countGrand(const Fence& fence, map<Fence, int>& fenceMap) {
-    if (fenceMap.count(fence)) {
-        return fenceMap[fence];
-    }
-
-    int grand = 0;
-    if (fence.seqCount() == 0) {
-        grand = 0;
-    } else {
-
-
-        set<int> grandNums;
-        for (short n : fence.getSeq()) {
-            for (int nn = 0; nn < n; ++nn) {
-                for (int downCount = 1; downCount <= 2; ++downCount) {
-                    if (nn <= n - downCount) {
-                        grandNums.emplace(countGrand(fence.push(n, nn, downCount), fenceMap));
-                    }
-                }
-            }
-        }
-        grand = mex(grandNums);
-    }
-
-
-    /*{
-        for (int n = 0; n < fence.seqCount(); ++n) {
-            for (int nn = 0; nn < fence.seqNSize(n); ++nn) {
-                cout << "|";
-            }
-            cout << "_";
-        }
-
-        cout << " : (" << grand << ")" << endl;
-    }*/
-
-
-    fenceMap.emplace(fence, grand);
-    return grand;
+    cout << "g("<<n<<") = " << grandNums[n] << endl;
+    return grandNums[n];
 }
 
 int main() {
-    int n = 0;
+    short n = 0;
     cin >> n;
 
-    Fence startFence(set<short>({ (short)n }));
-    map<Fence, int> fenceMap;
-
-    int grand = countGrand(startFence, fenceMap);
+    int grand = countFenceGrand(n);
+    // int grand = countGrand(startFence, fenceMap);
     // cout << "g(" << n << ") = " << grand << endl;
     // int grand = 1;
 
